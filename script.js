446663256447b3ad3909a04c274c0cb3acc3c2ed
Leah <leah.jl@proton.me>
2023-02-17 15:46:59 +0100

    add sound effects and animations

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const restart = document.querySelector('.restart');
 const cases = document.querySelectorAll(".case")
 let info = document.querySelector('.info');
 
-let playerMove = "x";
-let computerMove = "o";
+let playerMove = "🐻‍❄️";
+let computerMove = "🦭";
 
 let currentPlayer = false;
 
@@ -13,22 +13,24 @@ let currentPlayer = false;
 function play(i) {
 
     if (array[i] === "" && currentPlayer === false) {
-        array[i] = "x";
-        logInfo("It's player o's turn");
+        array[i] = playerMove;
+        logInfo("It's seal's turn");
     }
     else if (array[i] === "" && currentPlayer === true) {
-        array[i] = "o";
-        logInfo("It's player x's turn");
+        array[i] = computerMove;
+        logInfo("It's polar bear's turn");
     }
     currentPlayer = !currentPlayer;
     display();
     checkCombination();
+    sound();
 }
 
-//display players moves on board by transferring values from array(js) to cases(html)-----------//
+//display player's moves on board by transferring values from array(js) to cases(html)-----------//
 function display() {
     for (i = 0; i < array.length; i++) {
         cases[i].textContent = array[i];
+
     }
 }
 
@@ -39,84 +41,100 @@ function logInfo(m) {
 
 function checkCombination() {
     //wining combo for x--------------------------------------------------------------------------//
-    if (array[0] === "x" && array[1] === "x" && array[2] === "x") {
-        logInfo("player x has won !");
+    if (array[0] === playerMove && array[1] === playerMove && array[2] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[3] === "x" && array[4] === "x" && array[5] === "x") {
-        logInfo("player x has won !");
+    else if (array[3] === playerMove && array[4] === playerMove && array[5] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[6] === "x" && array[7] === "x" && array[8] === "x") {
-        logInfo("player x has won !");
+    else if (array[6] === playerMove && array[7] === playerMove && array[8] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[0] === "x" && array[3] === "x" && array[6] === "x") {
-        logInfo("player x has won !");
+    else if (array[0] === playerMove && array[3] === playerMove && array[6] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[1] === "x" && array[4] === "x" && array[7] === "x") {
-        logInfo("player x has won !");
+    else if (array[1] === playerMove && array[4] === playerMove && array[7] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[2] === "x" && array[5] === "x" && array[8] === "x") {
-        logInfo("player x has won !");
+    else if (array[2] === playerMove && array[5] === playerMove && array[8] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[0] === "x" && array[4] === "x" && array[8] === "x") {
-        logInfo("player x has won !");
+    else if (array[0] === playerMove && array[4] === playerMove && array[8] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[2] === "x" && array[4] === "x" && array[6] === "x") {
-        logInfo("player x has won !");
+    else if (array[2] === playerMove && array[4] === playerMove && array[6] === playerMove) {
+        logInfo("🐻‍❄️ has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
     //wining combo for o--------------------------------------------------------------------------//
-    else if (array[0] === "o" && array[1] === "o" && array[2] === "o") {
-        logInfo("player o has won !");
+    else if (array[0] === computerMove && array[1] === computerMove && array[2] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[3] === "o" && array[4] === "o" && array[5] === "o") {
-        logInfo("player o has won !");
+    else if (array[3] === computerMove && array[4] === computerMove && array[5] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[6] === "o" && array[7] === "o" && array[8] === "o") {
-        logInfo("player o has won !");
+    else if (array[6] === computerMove && array[7] === computerMove && array[8] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[0] === "o" && array[3] === "o" && array[6] === "o") {
-        logInfo("player o has won !");
+    else if (array[0] === computerMove && array[3] === computerMove && array[6] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[1] === "o" && array[4] === "o" && array[7] === "o") {
-        logInfo("player o has won !");
+    else if (array[1] === computerMove && array[4] === computerMove && array[7] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[2] === "o" && array[5] === "o" && array[8] === "o") {
-        logInfo("player o has won !");
+    else if (array[2] === computerMove && array[5] === computerMove && array[8] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[0] === "o" && array[4] === "o" && array[8] === "o") {
-        logInfo("player o has won !");
+    else if (array[0] === computerMove && array[4] === computerMove && array[8] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
-    else if (array[2] === "o" && array[4] === "o" && array[6] === "o") {
-        logInfo("player o has won !");
+    else if (array[2] === computerMove && array[4] === computerMove && array[6] === computerMove) {
+        logInfo("🦭 has won !");
         togglebuttons(true);
+        toggleAnimation();
     }
 
     else if (array[0] !== "" && array[1] !== "" && array[2] !== "" &&
@@ -135,6 +153,34 @@ function togglebuttons(b) {
     buttons.forEach(function (button) {
         button.disabled = b;
     })
+}
+
+//toggle beluga jump and make the message bigger when first player wins--------------------------//
+function toggleAnimation() {
+    let belugaJump = document.querySelector('.beluga');
+    belugaJump.classList.add('beluga_anim');
+    let waterDrops = document.querySelector('.water');
+    waterDrops.classList.add('water_anim');
+    let winLog = document.querySelector("h3");
+    winLog.classList.add('h3_anim');
+    let splash = document.querySelector('.splash');
+    splash.volume = 0.1;
+    splash.play();
+}
+
+//background sound-------------------------------------------------------------------------------//
+function sound() {
+    let sound = document.querySelector('.sea_sound');
+    sound.volume = 0.1;
+    sound.play();
+    let wind = document.querySelector('.wind_sound');
+    wind.volume = 0.5;
+    wind.play();
+}
+
+//splash sound-----------------------------------------------------------------------------------//
+function splash() {
+
 }
 
 
